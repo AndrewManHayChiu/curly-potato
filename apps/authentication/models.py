f2c0e92a927c4c5341dfaf6cca9393b0787b12b9
Andrew Chiu <amhchiu@gmail.com>
2022-04-29 01:14:15 +1000

    Update models.py with SQLalchemy model from baddy_game_organiser repo; change README

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -33,8 +33,78 @@ class Users(db.Model, UserMixin):
             setattr(self, property, value)
 
     def __repr__(self):
-        return str(self.username)
+        return f"<Username: {self.username}>"
 
+class Player(db.Model):
+    __tablename__ = 'players'
+    
+    player_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    gender = db.Column(db.String, nullable=True)
+    # tier = relationship("PlayerTier")
+    # game = relationship("Game", back_populates="player")
+    
+    # def __init__(self, player_id, name, gender):
+    #     self.player_id = player_id
+    #     self.name = name
+    #     self.gender = gender
+        
+    def __repr__(self):
+        return f"<Player name: {self.name}>"
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+    
+    venue_id = db.Column(db.Integer, primary_key=True)
+    venue_name = db.Column(db.String)
+    floor_type = db.Column(db.String)
+    
+    def __repr__(self):
+        return f"<Venue name: {self.venue_name}>"
+
+class Social(db.Model):
+    __tablename__ = 'socials'
+    
+    social_id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venues.venue_id"))
+    social_name = db.Column(db.String)
+    social_weekday = db.Column(db.String)
+    social_time = db.Column(db.String)
+    doubles = db.Column(db.Integer, default=1)
+    social_skill_levels = db.Column(db.String, default='open')
+    
+    # game = relationship("Game", back_populates="social")
+    
+    def __repr__(self):
+        return f"<Social name: {self.social_name}>"
+
+class Game(db.Model):
+    __tablename__ = 'games'
+    
+    game_id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.Integer, db.ForeignKey("socials.social_id"))
+    player_id = db.Column(db.Integer, db.ForeignKey("players.player_id"))
+    game_type = db.Column(db.String)
+    win = db.Column(db.Integer)
+    played_date = db.Column(db.DateTime(timezone=True))
+    
+    # player = relationship("Player", back_populates="game")
+    # social = relationship("Social", back_populates="game")
+    
+    def __repr__(self):
+        return f"<Game type: {self.game_type}>"
+
+# Separate player tiers table because tiers can change over time
+# and because not every socials may use tiers
+class PlayerTier(db.Model):
+    __tablename__ = 'player_tiers'
+    tier_id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey("players.player_id"), primary_key=True)
+    tier = db.Column(db.Integer)
+    updated_at_date = db.Column(db.DateTime(timezone=True))
+    
+    def __repr__(self):
+        return f"<Tier: {self.tier}>"
 
 @login_manager.user_loader
 def user_loader(id):
